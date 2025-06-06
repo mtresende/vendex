bd0e77f5f7eb30c5c0358080e0a977dfaa455c9b
Matheus Resende <matheus.resende2003@gmail.com>
2025-06-06 17:37:20 -0300

    adicionando integração com banco de dados

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'dbvendex'
+});
+
+db.connect();
+
+app.post('/compras', (req, res) => {
+    const { data_compra, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento } = req.body;
+    const sql = 'INSERT INTO compras (data_compra, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(sql, [data_compra, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento], (err, result) => {
+        if (err) throw err;
+        res.send('Compra realizada com sucesso');
+    });
+});
+
+app.post('/parceiro', (req, res) => {
+    const { tipo, cpf_cnpj, telefone, cep, cidade, estado, rua, numero, complemento } = req.body;
+    const sql = 'INSERT INTO parceiro (tipo, cpf_cnpj, telefone, cep, cidade, estado, rua, numero, complemento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [tipo, cpf_cnpj, telefone, cep, cidade, estado, rua, numero, complemento], (err, result) => {
+        if (err) throw err;
+        res.send('Parceiro cadastrado com sucesso');
+    });
+});
+
+app.post('/produto', (req, res) => {
+    const { id, nome, validade, codigo_barras, quantidade, categoria, tipo } = req.body;
+    
+    if (tipo === 'servico') {
+        const sql = 'INSERT INTO servico (id, nome, categoria) VALUES (?, ?, ?)';
+        db.query(sql, [id, nome, categoria], (err, result) => {
+            if (err) throw err;
+            res.send('Serviço cadastrado com sucesso');
+        });
+    } else {
+        const sql = 'INSERT INTO produto (id, nome, validade, codigo_barras, quantidade) VALUES (?, ?, ?, ?, ?)';
+        db.query(sql, [id, nome, validade, codigo_barras, quantidade], (err, result) => {
+            if (err) throw err;
+            res.send('Produto cadastrado com sucesso');
+        });
+    }
+});
+
+app.post('/usuario', (req, res) => {
+    const { nome, email, senha } = req.body;
+    const sql = 'INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)';
+    db.query(sql, [nome, email, senha], (err, result) => {
+        if (err) throw err;
+        res.send('Usuário cadastrado com sucesso');
+    });
+});
+
+app.post('/vendas', (req, res) => {
+    const { data_venda, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento } = req.body;
+    const sql = 'INSERT INTO vendas (data_venda, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(sql, [data_venda, nome_produto, quantidade, valor_total, forma_pagamento, prazo_pagamento], (err, result) => {
+        if (err) throw err;
+        res.send('Venda realizada com sucesso');
+    });
+});
